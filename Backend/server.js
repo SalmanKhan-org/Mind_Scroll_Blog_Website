bcd60e770baddb35cf3d1e2154e09b80e9896a8c
unknown <sohilkhan8383@gmail.com>
2025-05-30 12:12:07 +0530

    production ready app

--- Backend/server.js
+++ Backend/server.js
@@ -7,10 +7,13 @@ const { connectDB } = require("./db/connectDB");
 const { error } = require("./middlewares/error");
 const userRouter = require("./routes/userRoutes");
 const categoryRouter = require("./routes/categoryRoutes");
-const blogRouter = require('./routes/blogRoutes')
+const blogRouter = require('./routes/blogRoutes');
+const path = require("path");
 
 app.use(cookieParser());
 app.use(express.json());
+
+const DIRNAME = path.resolve();
 
 app.use(cors({
     origin: process.env.FRONTEND_URL,
@@ -21,9 +24,13 @@ app.use(cors({
 app.use("/api/v1", userRouter);
 app.use("/api/v1", categoryRouter);
 app.use("/api/v1", blogRouter);
-
 //error Middleware
 app.use(error);
+
+app.use(express.static(path.join(__dirname, "/Frontend/dist")));
+app.use("/*\w", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
+})
 
 const PORT = process.env.PORT;
 
